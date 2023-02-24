d44c3ac62b071a1ae5941c6493a66e6af9b8def9
Matthew Eskolin <matteskolin@gmail.com>
2023-02-24 13:51:12 -0600

    add stringtools module

---
+++ Modules/StringTools/COOKBOOK- search for string in files.ps1
@@ -0,0 +1,4 @@
+﻿Select-String -Path C:\Users\Matthew\Desktop\DesktopModules\* -Pattern 'administrator'
+
+
+Get-ChildItem C:\DesktopModules\* -Recurse | Select-String -Pattern "IsInRole(" -SimpleMatch
